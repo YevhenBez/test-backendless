9dc36f57b5a5f2b4fe22dd8de464f7f03eefb212
YevhenBez <byxgalter@bigmir.net>
2023-10-30 13:48:37 +0200

    I'm refactoring the code in the App.jsx file  (I'm doing everything I did in previous commits), adjusting it to the technical specifications. Test for Backendless
    I'm refactoring the code in the App.jsx file  (I'm doing everything I did in previous commits), adjusting it to the technical specifications. The only difference from the previous code is that I take the paths to the site page files from the tabs.json file   .  To implement this logic, I imported Navigate, useEffect, useState.  Then in useState I set the initial value of the array into which I will subsequently load the data from the tabs.json file.  Then in useEffect I got the data from the tabs.json file and immediately wrote it to the tabsData variable  .  Then in the sortedData variable I sorted the data from the tabs.json file by order value. Then I used map to access the sortedData array data (these are the paths to the site pages that I load using lazy , id et cetera). Also here I applied the condition, if i === 0 (zero index of the array), then this is the home page of the Dummy List. Then using Navigate I will redirect directly to the Dummy List page  .   Test for Backendless

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,45 @@
-import { Routes, Route } from 'react-router-dom';
-import { lazy } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import {useEffect, useState, lazy } from 'react';
 import Layout from '../components/layout';
-const HomeDummyList = lazy(() => import('./tabs/dummyList'));
-const DummyTable = lazy(() => import('./tabs/dummyTable'));
-const DummyChart = lazy(() => import('./tabs/dummyChart'));
 
 export const App = () => {
+  const [tabsData, setTabsData] = useState([]);
+
+  useEffect(() => {
+  async function fetchTabs() {
+    try {
+      const data = await import('../service/tabs.json');
+      setTabsData(data.tabs);
+    } catch (error) {
+      console.log(error.message);
+    } 
+  }
+  fetchTabs()
+  }, [])
+  
+  const sortedData = tabsData.sort((a, b) => a.order - b.order);
+
   return (
     <div    >
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<HomeDummyList />} />
-          <Route path="dummyList" element={<HomeDummyList />} />
-          <Route path="dummyTable" element={<DummyTable />} />
-          <Route path="dummyChart" element={<DummyChart />} />
+          {sortedData.map((tab, i)=> {
+              const Page = lazy(() => import(`./${tab.path}`));
+              if (i === 0) {
+                return (
+                  <>
+                    <Route key={tab.id} index element={ <Navigate to={tab.id} /> }/>
+                    <Route key={tab.id} path={tab.id}  element={<Page />} />
+                  </>
+                )
+              }
+              return (
+                <Route key={tab.id} path={tab.id} element={<Page />} />
+              )
+            })}
         </Route>
       </Routes>
     </div>
   );
 };
+
