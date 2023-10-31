4ecc56595314f65027ef07caaa451e5377442977
YevhenBez <115114239+YevhenBez@users.noreply.github.com>
2023-10-31 10:12:08 +0000

    Deploying to gh-pages from @ YevhenBez/test-backendless@8c91fe2358d7ea6b1dcf2ed7d8bf97a1f710d4ea 🚀

---
+++ static/js/122.cf8a2981.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[122],{122:function(e,t,n){n.r(t),n.d(t,{StyledDummyTableTd:function(){return i}});var r,a=n(168),i=n(867).ZP.td(r||(r=(0,a.Z)(["\n    font-weight: 700;\n    font-size: 64px;\n    line-height: 1.25;\n\n    font-family: Merriweather, serif;\n"])))}}]);
+//# sourceMappingURL=122.cf8a2981.chunk.js.map
