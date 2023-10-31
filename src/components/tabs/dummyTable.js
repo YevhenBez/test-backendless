8c91fe2358d7ea6b1dcf2ed7d8bf97a1f710d4ea
YevhenBez <byxgalter@bigmir.net>
2023-10-31 12:11:09 +0200

    I styled the text of the  DummyTable  page. Test for Backendless
    I styled the text of the  DummyTable  page. To do this I wrote the styles in the DummyTable.styled.js  file and then imported them into the   dummyTable.js  file and applied them.   Test for Backendless

--- src/components/tabs/dummyTable.js
+++ src/components/tabs/dummyTable.js
@@ -1,6 +1,8 @@
+import { StyledDummyTableTd } from './styles/dummyTable/DummyTable.styled';
+
 const DummyTable = () => {
     return (
-        <table><tr><td>Dummy</td><td>Table</td></tr></table>
+        <table><tr><StyledDummyTableTd>Dummy</StyledDummyTableTd><StyledDummyTableTd>Table</StyledDummyTableTd></tr></table>
     )
 }
 
---
+++ src/components/tabs/styles/dummyTable/DummyTable.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledDummyTableTd = styled.td`
+    font-weight: 700;
+    font-size: 64px;
+    line-height: 1.25;
+
+    font-family: Merriweather, serif;
+`;
